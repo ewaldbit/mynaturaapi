9fdffa68f43f1600ed959563f8061a417545694f
Ewald Bitencourt <ewaldbit@gmail.com>
2021-07-28 22:59:16 -0300

    Developed get customer by ID

--- src/main/java/com/terabytesoft/mynaturaapi/controller/ClientController.java
+++ src/main/java/com/terabytesoft/mynaturaapi/controller/ClientController.java
@@ -3,16 +3,14 @@ package com.terabytesoft.mynaturaapi.controller;
 import com.terabytesoft.mynaturaapi.dto.ClientDTO;
 import com.terabytesoft.mynaturaapi.dto.MessageResponseDTO;
 import com.terabytesoft.mynaturaapi.entity.Client;
+import com.terabytesoft.mynaturaapi.exception.ClientNotFoundException;
 import com.terabytesoft.mynaturaapi.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/client")
@@ -30,5 +28,15 @@ public class ClientController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createClient(@RequestBody @Valid ClientDTO clientDTO){
         return clientService.createClient(clientDTO);
+    }
+
+    @GetMapping
+    public List<ClientDTO> listAll() {
+        return clientService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public ClientDTO findById(@PathVariable Long id) throws ClientNotFoundException {
+        return clientService.findById(id);
     }
 }
--- src/main/java/com/terabytesoft/mynaturaapi/service/ClientService.java
+++ src/main/java/com/terabytesoft/mynaturaapi/service/ClientService.java
@@ -3,17 +3,22 @@ package com.terabytesoft.mynaturaapi.service;
 import com.terabytesoft.mynaturaapi.dto.ClientDTO;
 import com.terabytesoft.mynaturaapi.dto.MessageResponseDTO;
 import com.terabytesoft.mynaturaapi.entity.Client;
+import com.terabytesoft.mynaturaapi.exception.ClientNotFoundException;
 import com.terabytesoft.mynaturaapi.mapper.ClientMapper;
 import com.terabytesoft.mynaturaapi.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientService {
 
     private ClientRepository clientRepository;
 
-    private ClientMapper clientMapper = ClientMapper.INSTANCE;
+    private final ClientMapper clientMapper = ClientMapper.INSTANCE;
 
     @Autowired
     public ClientService(ClientRepository clientRepository) {
@@ -29,5 +34,19 @@ public class ClientService {
                 .builder()
                 .message("Client saved with id: " + savedClient.getId())
                 .build();
+    }
+
+    public List<ClientDTO> listAll() {
+        List<Client> allClients = clientRepository.findAll();
+        return allClients.stream()
+                .map(clientMapper::ToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public ClientDTO findById(Long id) throws ClientNotFoundException {
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> new ClientNotFoundException(id));
+
+        return clientMapper.ToDTO(client);
     }
 }
